b277a9b2f795da6566019dbe419f59406045161a
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-16 22:27:14 -0400

    feat(models): creacion de models
    primera estructura que se podria utilizar en el proyecto

---
+++ cartera/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Subcategoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.categoria})"
+
+
+class Fuente(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Ingreso(models.Model):
+    fecha = models.DateField()
+    fuente = models.ForeignKey(Fuente, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.categoria}: {self.cantidad}"
+
+
+class Gasto(models.Model):
+    fecha = models.DateField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    subcategoria = models.ForeignKey(
+        Subcategoria, on_delete=models.SET_NULL, blank=True, null=True
+    )
+    cantidad = models.DecimalField(max_digits=10, decimal_places=2)
+    metodo_pago = models.CharField(max_length=50)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.categoria}: {self.cantidad}"
