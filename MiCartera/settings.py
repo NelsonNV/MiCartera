b76cf646b3c4c13c2ec813a28d94c80cee0d1779
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-27 18:02:18 -0400

    feat: ajuste para usar humanize

--- MiCartera/settings.py
+++ MiCartera/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "cartera",
 ]
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es-mx"
 
 TIME_ZONE = "UTC"
 
