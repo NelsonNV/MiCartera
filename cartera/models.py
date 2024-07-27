18b92dfdc095cdc29e8ebd9546b2c693bd62cacc
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-27 11:28:46 -0400

    refactor(models): categoria con unique

--- cartera/models.py
+++ cartera/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(unique=True, max_length=50)
 
     def __str__(self):
         return str(self.nombre)
