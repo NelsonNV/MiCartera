fde31bd05848d7fda874c72ca41c51c894554347
NelsonNV <nelsonluisnv2@gmail.com>
2024-07-27 15:41:58 -0400

    feat(forms): winget y class agregadas

--- cartera/forms.py
+++ cartera/forms.py
@@ -6,18 +6,34 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = Categoria
         fields = ["nombre"]
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={"class": "input", "placeholder": "Nombre de la categoría"}
+            ),
+        }
 
 
 class SubcategoriaForm(forms.ModelForm):
     class Meta:
         model = Subcategoria
         fields = ["nombre", "categoria"]
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={"class": "input", "placeholder": "Nombre de la subcategoría"}
+            ),
+            "categoria": forms.Select(attrs={"class": "select"}),
+        }
 
 
 class FuenteForm(forms.ModelForm):
     class Meta:
         model = Fuente
         fields = ["nombre"]
+        widgets = {
+            "nombre": forms.TextInput(
+                attrs={"class": "input", "placeholder": "Nombre de la fuente"}
+            ),
+        }
 
 
 class IngresoForm(forms.ModelForm):
@@ -25,7 +41,15 @@ class IngresoForm(forms.ModelForm):
         model = Ingreso
         fields = ["fecha", "fuente", "categoria", "cantidad", "descripcion"]
         widgets = {
-            "fecha": forms.DateInput(attrs={"type": "date"}),
+            "fecha": forms.DateInput(attrs={"class": "input", "type": "date"}),
+            "fuente": forms.Select(attrs={"class": "select2 input"}),
+            "categoria": forms.Select(attrs={"class": "select2 input"}),
+            "cantidad": forms.NumberInput(
+                attrs={"class": "input", "placeholder": "Cantidad", "step": "0.01"}
+            ),
+            "descripcion": forms.Textarea(
+                attrs={"class": "textarea", "placeholder": "Descripción", "rows": 3}
+            ),
         }
 
 
@@ -41,5 +65,16 @@ class GastoForm(forms.ModelForm):
             "descripcion",
         ]
         widgets = {
-            "fecha": forms.DateInput(attrs={"type": "date"}),
+            "fecha": forms.DateInput(attrs={"class": "input", "type": "date"}),
+            "categoria": forms.Select(attrs={"class": "select"}),
+            "subcategoria": forms.Select(attrs={"class": "select"}),
+            "cantidad": forms.NumberInput(
+                attrs={"class": "input", "placeholder": "Cantidad", "step": "0.01"}
+            ),
+            "metodo_pago": forms.TextInput(
+                attrs={"class": "input", "placeholder": "Método de pago"}
+            ),
+            "descripcion": forms.Textarea(
+                attrs={"class": "textarea", "placeholder": "Descripción", "rows": 3}
+            ),
         }
